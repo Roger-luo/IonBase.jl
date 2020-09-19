8aee9dd28cb63c64b6e07a263acf3a451e3099ed
Roger-luo <rogerluo.rl18@gmail.com>
2020-09-19 14:57:03 -0400

    add snoope compile

--- deps/snoop_compile.jl
+++ deps/snoop_compile.jl
@@ -4,11 +4,7 @@ project = Pkg.project().path
 # This only needs to be run once (to generate "/tmp/comonicon_compiles.log")
 
 SnoopCompile.@snoopc ["--project=$project"] "/tmp/ion_compiles.log" begin
-    using IonBase, Comonicon
-    include(Comonicon.PATH.project(IonBase, "test", "runtests.jl"))
-    IonBase.search("Yao")
-
-    cd(Comonicon.PATH.project(IonBase, "test"))
+    include("precompile.jl")
 end
 
 ### Parse the compiles and generate precompilation scripts
