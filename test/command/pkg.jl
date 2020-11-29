1c65960258a14959a00339d51492cb2ae0a0c3ea
Roger-luo <rogerluo.rl18@gmail.com>
2020-11-29 14:27:18 -0500

    fix pkg test

--- test/command/pkg.jl
+++ test/command/pkg.jl
@@ -4,11 +4,13 @@ using IonBase
 
 include(joinpath(pkgdir(IonBase), "test", "utils.jl"))
 
-cd(joinpath(test_dir, "Foo")) do
-    IonBase.add("OhMyREPL")
-    IonBase.rm("OhMyREPL")
-    IonBase.instantiate()
-    IonBase.status()
+with_test_ion() do
+    cd(joinpath(test_dir, "Foo")) do
+        IonBase.add("OhMyREPL")
+        IonBase.rm("OhMyREPL")
+        IonBase.instantiate()
+        IonBase.status()
+    end
 end
 
 end
